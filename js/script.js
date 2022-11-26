d5e35521d536607b26dcb00e33bb67f14f02ef54
Your Name <you@example.com>
2022-11-26 17:43:17 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,71 @@
+"use strict";
+
+   console.log(1);
+   // Сообщение пользователю
+   console.log(alert("Hello user"));
+   // Вопрос пользователю
+   const result = confirm("Are you here?");
+   console.log(result);
+
+
+
+
+
+
+
+
+   
+const obj = {
+   objname:"Qwerty",
+   objage:"14",
+   objhobby:"Build"
+};
+   obj.name = "IT";
+   obj.age = ":)";
+   obj.hobby = "Will Destoy You";
+
+   console.log(obj.name);
+   console.log(obj.hobby);
+   console.log(obj.age);
+
+console.log(5 + 5);
+console.log(5 + '5');
+console.log('5' + +'5');
+console.log('arr' +'-object');
+console.log(5 * 5);
+
+console.log(5 / 5);
+
+let incr = '10';
+let decr = '10';
+
+console.log(incr++);
+console.log(decr--);
+
+
+++incr;
+--decr;
+
+console.log(++incr);
+console.log(--decr);
+console.log(5 & 2);
+const name = 'text';
+midname='abdakimov';
+
+console.log(5 + 3 ==8);
+console.log(5 + 3 == '8');
+
+
+
+const math = [];
+math[0] = 1+1;
+math[1] = 5-5;
+math[2] = 5/5;
+math[3] = 5*5;
+math[4] = 'dair';
+
+console.log(math);
+console.log(++incr[1]);
+console.log(--decr[2]);
+console.log('math[4]' + '-fall');
+
